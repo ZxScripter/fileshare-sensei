913773091e16c16faa4041ef7feb7e8eac49d6c9
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-08-16 05:43:10 +0530

    config.py

--- config.py
+++ config.py
@@ -13,23 +13,23 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6602960633:AAHUhTpbkkYiQWu5y7ZjEtONeJ0VpxAxC4Y")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "26376042"))
+APP_ID = int(os.environ.get("APP_ID", "26376042")
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "1f5343b0646645ca1eaf7c4759fc248f"))
+API_HASH = os.environ.get("API_HASH", "1f5343b0646645ca1eaf7c4759fc248f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001885067474"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001885067474")
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "2036803347"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "2036803347")
 
 #Port
-PORT = os.environ.get("PORT", "8080"))
+PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot"))
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001936280485"))
